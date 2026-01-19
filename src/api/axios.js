74565ca6e8a0f0bd5d1d0fc16e8ca474cdddfd34
Ibrohim Qosimov <ibrohim.2128382@gmail.com>
2026-01-19 10:57:22 +0500

    Update API base URL to production environment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:2000/api",
-    // baseURL: "https://icego.up.railway.app/api",
+    // baseURL: "http://localhost:2000/api",
+    baseURL: "https://icego.up.railway.app/api",
 
     headers: {
         "Content-Type": "application/json"
